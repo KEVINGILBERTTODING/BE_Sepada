916e218088d73704482dd426c72cb0c7465a57b0
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2023-06-18 01:44:25 +0700

    Added keloa tamu

---
+++ application/controllers/api/Admin.php
@@ -0,0 +1,77 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+
+class Admin extends CI_Controller
+{
+
+	public function __construct()
+	{
+		parent::__construct();
+
+		date_default_timezone_set('Asia/Jakarta');
+		$this->load->model('api/cuti_model');
+		$this->load->helper('download');
+		$this->load->model('api/user_model');
+		$this->load->model('api/user_detail_model');
+	}
+
+	function getAllPengajuan()
+	{
+		echo json_encode($this->cuti_model->getAllCuti());
+	}
+
+	function getAllPengajuanByStatus()
+	{
+		$status = $this->input->get('status');
+		echo json_encode($this->cuti_model->getAllCutiByStatus($status));
+	}
+	function downloadSuratLampiran($id)
+	{
+
+		$data = $this->cuti_model->getCutiById($id);
+		$filename = $data['file'];
+		$file = 'uploads/' . $filename;
+		force_download($file, NULL);
+	}
+
+	function updatePengajuan()
+	{
+		$id = $this->input->post('id');
+		$data = [
+			'alasan' => $this->input->post('alasan'),
+			'jam' => $this->input->post('jam'),
+			'tanggal' => $this->input->post('tanggal'),
+			'jumlah' => $this->input->post('jumlah'),
+		];
+		$update = $this->cuti_model->update($id, $data);
+		if ($update == true) {
+			$response = [
+				'code' => 200
+			];
+			echo json_encode($response);
+		} else {
+			$response = [
+				'code' => 404
+			];
+			echo json_encode($response);
+		}
+	}
+
+	function deletePengajuan()
+	{
+		$id = $this->input->post('id');
+		$delete = $this->cuti_model->delete($id);
+		if ($delete == true) {
+			$response = [
+				'code' => 200
+			];
+			echo json_encode($response);
+		} else {
+			$response = [
+				'code' => 404
+			];
+			echo json_encode($response);
+		}
+	}
+}
--- application/models/api/Cuti_model.php
+++ application/models/api/Cuti_model.php
@@ -22,6 +22,16 @@ class Cuti_model extends CI_Model
 		return $this->db->get()->result();
 	}
 
+	function getAllCuti()
+	{
+		$this->db->select('*');
+		$this->db->from('cuti');
+		$this->db->join('user', 'user.id_user = cuti. id_user', 'left');
+		$this->db->join('user_detail', 'user_detail.id_user_detail = cuti. id_user', 'left');
+		$this->db->order_by('cuti. tgl_diajukan', 'desc');
+		return $this->db->get()->result();
+	}
+
 	function getCutiByStatus($id, $status)
 	{
 		if ($status == 'all') {
@@ -44,6 +54,28 @@ class Cuti_model extends CI_Model
 		}
 	}
 
+
+	function getAllCutiByStatus($status)
+	{
+		if ($status == 'all') {
+			$this->db->select('*');
+			$this->db->from('cuti');
+			$this->db->join('user', 'user.id_user = cuti. id_user', 'left');
+			$this->db->join('user_detail', 'user_detail.id_user_detail = cuti. id_user', 'left');
+			$this->db->order_by('cuti. tgl_diajukan', 'desc');
+			return $this->db->get()->result();
+		} else {
+			$this->db->select('*');
+			$this->db->from('cuti');
+			$this->db->join('user', 'user.id_user = cuti. id_user', 'left');
+			$this->db->join('user_detail', 'user_detail.id_user_detail = cuti. id_user', 'left');
+			$this->db->where('cuti.id_status', $status);
+			$this->db->order_by('cuti. tgl_diajukan', 'desc');
+			return $this->db->get()->result();
+		}
+	}
+
+
 	function getCutiById($id)
 	{
 		$this->db->select('*');
@@ -59,6 +91,30 @@ class Cuti_model extends CI_Model
 	{
 		$insert = $this->db->insert('cuti', $data);
 		if ($insert) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function update($id, $data)
+	{
+
+		$this->db->where('id_tamu', $id);
+		$update = $this->db->update('cuti', $data);
+		if ($update) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	function delete($id)
+	{
+
+		$this->db->where('id_tamu', $id);
+		$delete = $this->db->delete('cuti');
+		if ($delete) {
 			return true;
 		} else {
 			return false;
